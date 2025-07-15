821dbf027ab70f9ff3ca32976676b06d4201352d
sonaiyasahil10 <sonaiyasahil10@gmail.com>
2025-07-15 23:17:33 +0530

    updates Task1 and sample file

--- Task1.py
+++ Task1.py
@@ -1,13 +1,13 @@
 #Task 1: Read a File and Handle Errors
 try:
-    file = open("sample.txt", "r") #modes specified for further operations
+    file = open("sample1.txt", "r") #modes specified for further operations
     read_file= file.readlines()
     for lines in read_file:
         print(lines)
     file.close()
 
 except FileNotFoundError:
-    print("The file \"sample.txt\" was not found")
+    print("Error: The file \"sample.txt\" was not found.")
 
 # with open("sample.txt", "r") as file:
 #     read_file = file.readlines()
